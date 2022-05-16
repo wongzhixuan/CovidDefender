cc9a120dea75d2967fc6205987d5e2553b3f9465
wongzhixuan <annie13075@gmail.com>
2022-05-16 22:09:16 +0800

    bottom_nav

--- app/src/main/java/com/example/coviddefender/BottomNav.kt
+++ app/src/main/java/com/example/coviddefender/BottomNav.kt
@@ -2,10 +2,29 @@ package com.example.coviddefender
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BottomNav : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_nav)
+        val navController = this.findNavController(R.id.nav_host_fragment_activity_main)
+
+        // find reference to bottom navigation view
+        val navView: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        // Hook your navigation controller to bottom navigation view
+        navView.setupWithNavController(navController)
+        navView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent))
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener(View.OnClickListener {
+            navController.navigate(R.id.scan)
+        })
     }
 }
--- app/src/main/java/com/example/coviddefender/LoadScreen.kt
+++ app/src/main/java/com/example/coviddefender/LoadScreen.kt
@@ -1,5 +1,6 @@
 package com.example.coviddefender
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,9 @@ class LoadScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load_screen)
+
+        //Test code
+        val intent = Intent(this, BottomNav::class.java)
+        startActivity(intent)
     }
 }
