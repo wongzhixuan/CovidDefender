12ba759fbf7e7f90d35b0fc1f3e38d70546f1b89
wongzhixuan <annie13075@gmail.com>
2022-05-16 21:10:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/coviddefender/LoadScreen.kt
@@ -0,0 +1,11 @@
+package com.example.coviddefender
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_screen)
+    }
+}
