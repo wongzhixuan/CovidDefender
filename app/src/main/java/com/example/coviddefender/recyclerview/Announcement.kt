528915b518f85091b8ec2671298558cf30a88c13
wongzhixuan <annie13075@gmail.com>
2022-05-17 21:41:38 +0800

    home_fragment_design

---
+++ app/src/main/java/com/example/coviddefender/recyclerview/Announcement.kt
@@ -0,0 +1,8 @@
+package com.example.coviddefender.recyclerview
+
+import android.widget.ImageView
+
+data class Announcement (
+        var thumbnail: ImageView,
+        var description: String
+        )
