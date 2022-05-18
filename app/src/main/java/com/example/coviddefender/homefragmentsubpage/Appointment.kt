7e308e19aa5bc0c7a3a4c71286830921edc8aadc
wongzhixuan <annie13075@gmail.com>
2022-05-18 21:43:07 +0800

    Home_fragment subpages progressing

--- app/src/main/java/com/example/coviddefender/homefragmentsubpage/Appointment.kt
+++ app/src/main/java/com/example/coviddefender/homefragmentsubpage/Appointment.kt
@@ -23,10 +23,10 @@ class Appointment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.fragment_covid_status, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_appointment, container, false)
         val btn_back : ImageButton = view.findViewById<ImageButton>(R.id.btn_back)
         btn_back?.setOnClickListener(View.OnClickListener {
-            findNavController().navigate(R.id.action_covid_status_to_home)
+            findNavController().navigate(R.id.action_appointment_to_home)
 
         })
         return view
