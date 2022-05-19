bb331491c58a82ccfcbe8bea9909e8599c7a8f75
wongzhixuan <annie13075@gmail.com>
2022-05-19 22:20:57 +0800

    groupcheckin_fragment partially done

--- app/src/main/java/com/example/coviddefender/homefragmentsubpage/GroupCheckIn.kt
+++ app/src/main/java/com/example/coviddefender/homefragmentsubpage/GroupCheckIn.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.coviddefender.R
+import com.example.coviddefender.recyclerview.Dependent
+import com.example.coviddefender.recyclerview.DependentAdapter
+import com.example.coviddefender.recyclerview.HistoryAdapter
 
 class GroupCheckIn : Fragment() {
 
@@ -22,6 +27,31 @@ class GroupCheckIn : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_group_check_in, container, false)
+
+        // Dummy data for recycler view
+        var dependents: ArrayList<Dependent> = arrayListOf(
+            Dependent("Cloud Strife", "Friend", true),
+            Dependent("Cloud Strife", "Friend", false),
+            Dependent("Cloud Strife", "Friend", false),
+            Dependent("Cloud Strife", "Friend", false),
+            Dependent("Cloud Strife", "Friend", false),
+            Dependent("Cloud Strife", "Friend", false),
+            Dependent("Cloud Strife", "Friend", false),
+            Dependent("Cloud Strife", "Friend", false),
+
+        )
+        // Dependent Recycler View
+        val dependent_recyclerview: RecyclerView = view.findViewById<RecyclerView>(R.id.dependent_recyclerview)
+        dependent_recyclerview.layoutManager = LinearLayoutManager(
+            view.context,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+        // adopt data to recycler view using adapter
+        dependent_recyclerview.adapter = DependentAdapter(dependents)
+
+
         val btn_back : ImageButton = view.findViewById<ImageButton>(R.id.btn_back)
         btn_back?.setOnClickListener(View.OnClickListener {
             findNavController().navigate(R.id.action_groupCheckIn_to_home)
---
+++ app/src/main/java/com/example/coviddefender/recyclerview/Dependent.kt
@@ -0,0 +1,7 @@
+package com.example.coviddefender.recyclerview
+
+data class Dependent (
+    var dependent_name: String,
+    var dependent_relation: String,
+    var checked: Boolean
+        )
