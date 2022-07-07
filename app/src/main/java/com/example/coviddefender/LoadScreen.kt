4c9595a290c77c6d1553c590fa716a93377d93cf
lishien1210 <57692804+lishien1210@users.noreply.github.com>
2022-07-07 17:09:05 +0800

    theme

--- app/src/main/java/com/example/coviddefender/LoadScreen.kt
+++ app/src/main/java/com/example/coviddefender/LoadScreen.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.coviddefender.UserAuthentication.LoginActivity
 
 class LoadScreen : AppCompatActivity() {
     var loading_time = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load_screen)
 
